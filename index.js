138adc3eec7657ace25dcb7e2e288b574b286500
kashif-ali-khan <kashif.7374@gmail.com>
2022-02-08 17:29:09 +0530

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 // Import stylesheets
 import './style.css';
+import data from './json/data';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById('title');
+console.log(data);
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+generatePostCommentSection();
+attachPostCommentListener();
+
+//generateListOfComment();
+function attachPostCommentListener(){
+  document.querySelector("#postComment").addEventListener('click', (event)=>{
+    console.log(event.target.tagName)
+    if(event.target.tagName === 'BUTTON' && document.querySelector("#commentValue").value ){
+      console.log(document.querySelector("#commentValue").value)
+
+      generateListOfComment(document.querySelector("#commentValue").value);
+      document.querySelector("#commentValue").value = '';
+    }
+  })
+}
+
+function generateListOfComment(comment){
+  var commentDiv = document.createElement('div');
+  commentDiv.innerHTML = comment;
+  commentDiv.className = 'comment';
+  document.querySelector(".commentsList").appendChild(commentDiv);
+  document.querySelector(".commentsList").appendChild(document.createElement('hr'));
+
+
+}
+function generatePostCommentSection(){
+
+  var comment = `
+  <div>
+    <div><textarea placeholder="Enter Comment" name="commentValue" id="commentValue"></textarea></div>
+    <div><button>Post Comment</button></div>
+  </div>
+  `;
+
+  document.querySelector("#postComment").innerHTML = comment;
+
+}
+
+
---
+++ json/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: '1',
+    name: 'kashif First Comment',
+    children: [
+      {
+        id: '2',
+        name: 'Test Child 1',
+      },
+      {
+        id: '3',
+        name: 'Test Child 2',
+      },
+    ],
+  },
+  {
+    id: '4',
+    name: 'kashif Second Comment',
+    children: [
+      {
+        id: '5',
+        name: 'Test Child 3',
+      },
+      {
+        id: '6',
+        name: 'Test Child 4',
+      },
+    ],
+  },
+];
+
+export default data;
