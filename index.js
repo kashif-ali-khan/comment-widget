b588da0348f990ffb7fbc338e2dcdb63c837218d
kashif-ali-khan <kashif.7374@gmail.com>
2022-02-08 17:51:46 +0530

    2 files modified

--- index.js
+++ index.js
@@ -8,32 +8,60 @@ console.log(data);
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 generatePostCommentSection();
-attachPostCommentListener();
+attachListener();
 
 //generateListOfComment();
-function attachPostCommentListener(){
-  document.querySelector("#postComment").addEventListener('click', (event)=>{
-    console.log(event.target.tagName)
-    if(event.target.tagName === 'BUTTON' && document.querySelector("#commentValue").value ){
-      console.log(document.querySelector("#commentValue").value)
+function attachListener() {
+  document.querySelector('#postComment').addEventListener('click', (event) => {
+    console.log(event.target.tagName);
+    if (
+      event.target.tagName === 'BUTTON' &&
+      document.querySelector('#commentValue').value
+    ) {
+      console.log(document.querySelector('#commentValue').value);
 
-      generateListOfComment(document.querySelector("#commentValue").value);
-      document.querySelector("#commentValue").value = '';
+      generateListOfComment(document.querySelector('#commentValue').value);
+      document.querySelector('#commentValue').value = '';
     }
-  })
+  });
+
+  document.querySelector('.commentsList').addEventListener('click', (event) => {
+    console.log(event.target.parentNode);
+    if (event.target.tagName === 'A') {
+      removeComment(event.target.parentNode);
+    }
+  });
 }
 
-function generateListOfComment(comment){
+function removeComment(node) {
+  document.querySelector('.commentsList').removeChild(node);
+}
+
+function generateListOfComment(comment) {
   var commentDiv = document.createElement('div');
-  commentDiv.innerHTML = comment;
+
+  /////
+  var commentSpan = document.createElement('span');
+  commentSpan.innerHTML = comment;
+  commentDiv.appendChild(commentSpan);
+  //////
+
+  var deleteLink = document.createElement('a');
+  deleteLink.className = 'delete-link';
+  deleteLink.innerHTML = 'Delete';
+  deleteLink.href = '#';
+  commentDiv.appendChild(deleteLink);
+
+  //////
+  commentDiv.appendChild(document.createElement('hr'));
+
+  ////
+
   commentDiv.className = 'comment';
-  document.querySelector(".commentsList").appendChild(commentDiv);
-  document.querySelector(".commentsList").appendChild(document.createElement('hr'));
-
-
+  document.querySelector('.commentsList').appendChild(commentDiv);
+ 
 }
-function generatePostCommentSection(){
-
+function generatePostCommentSection() {
   var comment = `
   <div>
     <div><textarea placeholder="Enter Comment" name="commentValue" id="commentValue"></textarea></div>
@@ -41,8 +69,5 @@ function generatePostCommentSection(){
   </div>
   `;
 
-  document.querySelector("#postComment").innerHTML = comment;
-
+  document.querySelector('#postComment').innerHTML = comment;
 }
-
-
